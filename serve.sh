b101dd374e0dba56e603d9f20b8a1cf3ae4423b4
João Escudero <joaovescudero@gmail.com>
2022-03-09 21:44:00 -0300

    fix(serve): removing shell start from serve.sh

--- serve.sh
+++ serve.sh
@@ -1,6 +1,5 @@
 echo "> Ensure you have deployed the project once before before emulating locally"
 cd ./functions
 firebase functions:config:get > .runtimeconfig.json
-firebase functions:shell
 cd ..
 firebase emulators:start
